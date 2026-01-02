136ff41221c64722a6e8e3b241bb4cc59b01e027
Shubham Kumar <shubham620409@gmail.com>
2026-01-02 23:28:37 +0530

    Admin Panel Add item completed

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -48,8 +48,18 @@ const removeFood = async(req, res)=>{
         fs.unlink(`uploads/${food.image}`,()=>{})
 
         await foodModel.findByIdAndDelete(req.body.id);
+        res.json(
+            {
+                success:true, 
+                message:"Food removed"
+            }
+        )
     } catch (error) {
-        
+        console.log(error);
+        res.json({
+            success:false, 
+            message:"Error"
+        })
     }
 }
 
