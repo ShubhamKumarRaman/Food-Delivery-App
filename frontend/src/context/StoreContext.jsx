4c59b49c64b8b302405b058694c894e4b328128a
Shubham Kumar <shubham620409@gmail.com>
2026-05-05 15:42:45 +0530

    Provide backend url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({})
 
-    const url = "http://localhost:4000"
+    const url = "https://food-delivery-backend-pllh.onrender.com"
 
     const [token, setToken] = useState("")
 
